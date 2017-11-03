09c1309f5ca7ce2ee2e9349bda2ae0ee5221f256
michael <m.coquet2@gmail.com>
2017-11-02 20:54:29 -0600

    adds stubs for LevelMenu onClick events

--- airlings/src/main/java/com/herophus/airlings/LevelMenu.java
+++ airlings/src/main/java/com/herophus/airlings/LevelMenu.java
@@ -2,6 +2,7 @@ package com.herophus.airlings;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class LevelMenu extends AppCompatActivity {
@@ -17,5 +18,45 @@ public class LevelMenu extends AppCompatActivity {
         getSupportActionBar().hide();
 
         setContentView(R.layout.activity_level_menu);
+    }
+
+    public void l1_onClick(View v) {
+
+    }
+
+    public void l2_onClick(View v) {
+
+    }
+
+    public void l3_onClick(View v) {
+
+    }
+
+    public void l4_onClick(View v) {
+
+    }
+
+    public void l5_onClick(View v) {
+
+    }
+
+    public void l6_onClick(View v) {
+
+    }
+
+    public void l7_onClick(View v) {
+
+    }
+
+    public void l8_onClick(View v) {
+
+    }
+
+    public void l9_onClick(View v) {
+
+    }
+
+    public void l10_onClick(View v) {
+
     }
 }
