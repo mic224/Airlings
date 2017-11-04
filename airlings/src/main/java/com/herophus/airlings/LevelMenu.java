abc4c42e240036b72dc0cd6739a0214e7d896114
michael <m.coquet2@gmail.com>
2017-11-04 03:30:05 -0600

    adds dynamic constraint based simple layout for the level menu
    completed a basic dynamic layout which currently only displays the first 5 stars, need to finish the entire layout with all the buttons and rank stars and titles

--- airlings/src/main/java/com/herophus/airlings/LevelMenu.java
+++ airlings/src/main/java/com/herophus/airlings/LevelMenu.java
@@ -1,6 +1,8 @@
 package com.herophus.airlings;
 
 import android.content.SharedPreferences;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,7 +15,8 @@ public class LevelMenu extends AppCompatActivity {
     public static final String currentLevel = "currentLevel";
 
     private static final int numberOfLevels = 10;
-    public static final String[] rankings = new String[10];
+    public static final String[] rankings = new String[numberOfLevels];
+    private ImageView[] ivStars = new ImageView[numberOfLevels];
 
     private int curLevel = 0;
 
@@ -46,7 +49,7 @@ public class LevelMenu extends AppCompatActivity {
             initRankings(editor);
         }
 
-        initStars();
+        initLayout();
 
     }
 
@@ -66,8 +69,51 @@ public class LevelMenu extends AppCompatActivity {
         editor.commit();
     }
 
-    private void initStars() {
+    private void initLayout() {
+        ConstraintLayout cl = findViewById(R.id.level_menu_layout);
+        ConstraintSet cs = new ConstraintSet();
 
+        // loop the first half of the levels
+        for(int i = 0; i < numberOfLevels/2; i++) {
+            ivStars[i] = new ImageView(this);
+            // set the id for connections
+            ivStars[i].setId(i);
+
+            // dont forget to add the ImageView to the layout
+            cl.addView(ivStars[i]);
+            // sets the constraint parameters with 50dp x 25dp to match the images
+            ConstraintLayout.LayoutParams cp = new ConstraintLayout.LayoutParams(dp(50),
+                    dp(25));
+
+            // constrain the ImageViews dimensions
+            cs.constrainWidth(ivStars[i].getId(), dp(50));
+            cs.constrainHeight(ivStars[i].getId(), dp(25));
+            if( i > 0) {
+
+                // constrain the image view to the center of the screen
+//                cs.center(ivStars[i].getId(), ConstraintSet.PARENT_ID, ConstraintSet.LEFT,
+//                        0, ConstraintSet.PARENT_ID, ConstraintSet.RIGHT,
+//                        0, 0.5f);
+
+                // connect this ImageView with the next one in the list
+                cs.connect(ivStars[i-1].getId(), ConstraintSet.LEFT, ivStars[i].getId(),
+                        ConstraintSet.RIGHT, 16);
+
+            } else {
+                cs.connect(ivStars[i].getId(), ConstraintSet.TOP, cl.getId(),
+                        ConstraintSet.TOP, 0);
+            }
+            // apply the constraints to the layout
+            cs.applyTo(cl);
+            // setting an image just for testing right now
+            ivStars[i].setImageResource(R.drawable.empty_star);
+        }
+    }
+
+    // returns the dp value for different density screens
+    private int dp(int dpValue) {
+        float d = this.getResources().getDisplayMetrics().density;
+        return (int)(dpValue * d);
     }
 
     public void l1_onClick(View v) {
